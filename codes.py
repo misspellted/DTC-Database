385c91d3fa810a75d0be24cb155aa6bc9265aaeb
James Goodrich <jdgoodrichjr@gmail.com>
2019-02-27 23:11:03 -0600

    Adds a sixth JSON data structure.
    Adds documentation to each method that generates a JSON data structure, so
    that the script need not be run six times to determine which the desired
    JSON data structure.
    
    The structures have fetched a rename as well (method names, prefix 'xmlTo'):
    
    * "All the Fields" -> JsonObject
    * "Unnamed Array/Verbose" -> VerboseJsonArray
    * "Named Array/Verbose" -> NamedVerboseJsonArray
    * "Unnamed Array/Compressed" -> CompressedJsonArray
    * "Named Array/Compressed" -> NamedCompressedJsonArray
    
    Note that the "Compressed" arrays do not "minimalize" the file, in that
    individual elements take up a minimum of 3 lines:
    
    {
    	"P1000": "Example code"
    }
    
    Minimalization can be affected by removing any leading or trailing whitespace
    characters per line (as all text descriptions of a code are single-line), but
    this functionality is not included at this time.

--- codes.py
+++ codes.py
@@ -1,35 +1,139 @@
 import xml.etree.ElementTree
 import json
 
+CODE = "code"
+TEXT = "text"
+
 def xmlRoot(xmlFilePath):
+    """Parses an XML file and returns the root element."""
     return xml.etree.ElementTree.parse(xmlFilePath).getroot()
 
-def xmlToJson_namedArray_verbose(xmlCodesRoot):
-    return {xmlCodesRoot.tag: [{"code": xmlCode.attrib["code"], "text": xmlCode.text.strip()} for xmlCode in xmlCodesRoot]}
+def xmlToTuple(xmlCode):
+    """Extracts the attribute and contents of a <code> element as a tuple."""
+    return (xmlCode.attrib[CODE], xmlCode.text.strip())
 
-def xmlToJson_namedArray_compressed(xmlCodesRoot):
-    return {xmlCodesRoot.tag: [{xmlCode.attrib["code"]: xmlCode.text.strip()} for xmlCode in xmlCodesRoot]}
+def xmlToJsonObject(xmlCodesRoot):
+    """Generates an unnamed fields dictionary in preparation for json.dump().
 
-def xmlToJson_unnamedArray_verbose(xmlCodesRoot):
-    return [{"code": xmlCode.attrib["code"], "text": xmlCode.text.strip()} for xmlCode in xmlCodesRoot]
+    Using the dictionary in the json.dump() call will produce the following
+    structure in the codes.json file:
 
-def xmlToJson_unnamedArray_compressed(xmlCodesRoot):
-    return [{xmlCode.attrib["code"]: xmlCode.text.strip()} for xmlCode in xmlCodesRoot]
-
-def xmlToJson_allTheFields(xmlCodesRoot):
-    fields = dict()
+    {
+        "P1000": "Example code",
+        ...
+    }
+    """
+    object = dict()
 
     for xmlCode in xmlCodesRoot:
-        fields[xmlCode.attrib["code"]] = xmlCode.text.strip()
+        field, value = xmlToTuple(xmlCode)
+        object[field] = value
 
-    return fields
+    return object
+
+def xmlToNamedJsonObject(xmlCodesRoot):
+    """Generates a named fields dictionary in preparation for json.dump().
+
+    Using the dictionary in the json.dump() call will produce the following
+    structure in the codes.json file:
+
+    {
+        "codes":
+        {
+            "P1000": "Example code",
+            ...
+        }
+    }
+    """
+    return {xmlCodesRoot.tag: xmlToJsonObject(xmlCodesRoot)}
+
+def xmlToVerboseJsonArray(xmlCodesRoot):
+    """Generates an unnamed fields list in preparation for json.dump().
+
+    Using the list in the json.dump() call will produce the following structure
+    in the codes.json file:
+
+    [
+        {
+            "code": "P1000",
+            "text": "Example code"
+        },
+        ...
+    ]
+    """
+    array = list()
+
+    for xmlCode in xmlCodesRoot:
+        field, value = xmlToTuple(xmlCode)
+        array.append({CODE: field, TEXT: value})
+
+    return array
+
+def xmlToNamedVerboseJsonArray(xmlCodesRoot):
+    """Generates a named fields list in preparation for json.dump().
+
+    Using the list in the json.dump() call will produce the following structure
+    in the codes.json file:
+
+    {
+        "codes":
+        [
+            {
+                "code": "P1000",
+                "text": "Example code"
+            },
+            ...
+        ]
+    }
+    """
+    return {xmlCodesRoot.tag: xmlToVerboseJsonArray(xmlCodesRoot)}
+
+def xmlToCompressedJsonArray(xmlCodesRoot):
+    """Generates an unnamed fields list in preparation for json.dump().
+
+    Using the list in the json.dump() call will produce the following structure
+    in the codes.json file:
+
+    [
+        {
+            "P1000": "Example code"
+        },
+        ...
+    ]
+    """
+    array = list()
+
+    for xmlCode in xmlCodesRoot:
+        field, value = xmlToTuple(xmlCode)
+        array.append({field: value})
+
+    return array
+
+def xmlToNamedCompressedJsonArray(xmlCodesRoot):
+    """Generates a named fields list in preparation for json.dump().
+
+    Using the list in the json.dump() call will produce the following structure
+    in the codes.json file:
+
+    {
+        "codes":
+        [
+            {
+                "P1000": "Example code"
+            },
+            ...
+        ]
+    }
+    """
+    return {xmlCodesRoot.tag: xmlToCompressedJsonArray(xmlCodesRoot)}
 
 def xmlToJsonCodesFile(xmlCodesRoot, jsonFilePath):
-#    xmlToJson = xmlToJson_namedArray_verbose
-#    xmlToJson = xmlToJson_namedArray_compressed
-    xmlToJson = xmlToJson_unnamedArray_verbose
-#    xmlToJson = xmlToJson_unnamedArray_compressed
-#    xmlToJson = xmlToJson_allTheFields
+#    xmlToJson = xmlToJsonObject
+    xmlToJson = xmlToNamedJsonObject
+#    xmlToJson = xmlToVerboseJsonArray
+#    xmlToJson = xmlToNamedVerboseJsonArray
+#    xmlToJson = xmlToCompressedJsonArray
+#    xmlToJson = xmlToNamedCompressedJsonArray
 
     with open(jsonFilePath, "w") as jsonCodesFile:
         json.dump(xmlToJson(xmlCodesRoot), jsonCodesFile, separators=(",",": "), indent=4)
